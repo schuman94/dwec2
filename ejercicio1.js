26edf73b5d0fc7b8d7c3ab25d173593c39c1215a
sergio <sergio.chulian@iesdonana.org>
2021-10-13 20:53:18 +0200

    resuelto

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+//Declaramos las variables y le damos un valor
+let entero = 1357;
+let decimal = 135.7;
+let cientifico = 135e7;
+let octal = 0o1357;
+let hexadecimal = 0x1357;
+
+//Mostramos en una ventana los numeros
+alert("Numero entero " + entero);
+alert("Numero decimal " + decimal);
+alert("Numero cientifico " + cientifico);
+alert("Numero octal " + octal);
+alert("Numero hexadecimal " + hexadecimal);
