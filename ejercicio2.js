c0ce68f47a19eef10577b3516d84ec780c9f3692
sergio <sergio.chulian@iesdonana.org>
2021-10-13 21:17:43 +0200

    ej2 terminado

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+let nombre = "Sergio";
+let apellido = "Chulian";
+let edad = 27;
+let nac = 1994;
+
+alert("'Hola mundo'");
+alert(nombre + "\n" + apellido);
+alert(edad + nac);
+alert(nombre + apellido + edad + nac);
