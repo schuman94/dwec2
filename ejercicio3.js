cab07443cadbc011db1d0cd79a9beab6d2b909ae
sergio <sergio.chulian@iesdonana.org>
2021-10-14 21:10:54 +0200

    14oct

---
+++ ejercicio3.js
@@ -0,0 +1,20 @@
+let op1 = (10 == 10);
+let op2 = (10 === 10);
+let op3 = (10 === 10.0);
+let op4 = ("Laura" == "Laura");
+let op5 = ("Laura" > "laura");
+let op6 = ("Laura" < "laura");
+let op7 = ("123" == 123);
+let op8 = ("123" === 123);
+let op9 = (parseInt("123") === 123);
+
+alert("La operación 10 == 10 es " + op1);
+alert("La operación 10 === 10 es " + op2);
+alert("La operación 10 === 10.0 es " + op3);
+alert("La operación 'Laura' == 'Laura' es " + op4);
+alert("La operación 'Laura' > 'laura' es " + op5);
+alert("La operación 'Laura' < 'laura' " + op6);
+alert("La operación '123' == 123 es " + op7);
+alert("La operación '123' === 123 es " + op8);
+alert("La operación parseInt('123') === 123 es " + op9);
+
